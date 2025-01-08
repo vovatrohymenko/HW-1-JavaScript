9681119e4f7cc8ad0f802612c4bbca5593dcb00a
vovatrohymenko <157517964+vovatrohymenko@users.noreply.github.com>
2025-01-08 21:42:22 +0200

    Домашня робота №1 JavaScript

---
+++ src/js/script.js
@@ -0,0 +1,31 @@
+"use strict";
+// №1
+const age = 15;
+console.log(age);
+// №2
+const name = "Вова";
+console.log(name);
+// №3
+const isStudent = true;
+console.log(isStudent);
+// №4
+const mySting = "Хто не знає свого минулого, той не вартий майбутнього.";
+console.log(mySting);
+// №5
+const myNumber = 28;
+const sumNumber = myNumber + 10;
+console.log(sumNumber);
+// №6
+const myNull = null;
+console.log(myNull);
+// №7
+const userName = prompt("Введіть своє ім’я");
+alert(userName);
+console.log(typeof userName);
+// №8
+const userAnswer = confirm("Ви дійсно хочете покинути сторінку?");
+console.log(userAnswer);
+// №9
+alert("Увага! Дія може бути небезпечною.");
+const websiteWarning = confirm("Ви впевнені, що хочете продовжити?");
+console.log(websiteWarning);
